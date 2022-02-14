90b07a590bdf2296bee16c782f1ce118277b8bb0
Shane Urbas <98282721+ShaneUrbas@users.noreply.github.com>
2022-02-13 19:13:05 -0500

    added punching
    added player punching and enemy death.

--- Project/Source/Player/Player.gd
+++ Project/Source/Player/Player.gd
@@ -1,28 +1,44 @@
 extends KinematicBody2D
 
 export var speed = 200
-
 var velocity = Vector2()
-
-signal animate
+var isAttacking = false
 
 func _physics_process(delta: float) -> void:
 	get_input()
-	animate()
+	punch()
 	velocity = move_and_slide(velocity)
-	
 
 func get_input():
 	velocity = Vector2()
-	if Input.is_action_pressed("left"):
+	if Input.is_action_pressed("left") && isAttacking == false:
 		velocity.x -= 1
-	if Input.is_action_pressed("right"):
+		$PlayerAnimation.play("walk")
+		scale.x = scale.y * -1
+	elif Input.is_action_pressed("right") && isAttacking == false:
 		velocity.x += 1
-	if Input.is_action_pressed("up"):
+		$PlayerAnimation.play("walk")
+		scale.x = scale.y * 1
+	elif Input.is_action_pressed("up") && isAttacking == false:
 		velocity.y -= 1
-	if Input.is_action_pressed("down"):
+		$PlayerAnimation.play("up")
+	elif Input.is_action_pressed("down") && isAttacking == false:
 		velocity.y += 1
+		$PlayerAnimation.play("walk")
+	else:  
+		if isAttacking == false:
+			$PlayerAnimation.play("idle")
+			
 	velocity = velocity.normalized() * speed
 	
-func animate():
-	emit_signal("animate", velocity)
+func punch():
+	if Input.is_action_just_pressed("punch"):
+		$PlayerAnimation.play("punch")
+		isAttacking = true;
+		$PlayerAnimation/PunchCollision/CollisionShape2D.disabled = false;
+
+func _on_PlayerAnimation_animation_finished() -> void:
+	if $PlayerAnimation.animation == "punch":
+		isAttacking = false;
+		$PlayerAnimation/PunchCollision/CollisionShape2D.disabled = true;
+
--- Project/Source/Player/PlayerAnimation.gd
+++ Project/Source/Player/PlayerAnimation.gd
@@ -1,12 +1,18 @@
 extends AnimatedSprite
 
+onready var punch = $PunchHit
+
+
+
 func _on_Player_animate(velocity) -> void:
 	if velocity.x < 0:
 		play("walk")
 		flip_h = true
+		punch.scale.x = scale.y * -1
 	elif velocity.x > 0:
 		play("walk")
 		flip_h = false
+		punch.scale.x = scale.y * 1
 	elif velocity.y > 0:
 		play("walk")
 		flip_h = false
@@ -14,4 +20,3 @@ func _on_Player_animate(velocity) -> void:
 		play("up")
 	else:
 		play("idle")
-
